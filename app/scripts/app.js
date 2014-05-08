b16704d85f64c4d0901fdd9c61108777031149c3
Nathan Friend <contact@nathanfriend.com>
2014-05-07 21:35:23 -0500

    added home link

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,11 +10,11 @@ angular.module('portfolio', [
     'portfolio.controllers'
 ]).
 config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', { templateUrl: 'views/landing.html', controller: 'LandingController' });
+    $routeProvider.when('/home', { templateUrl: 'views/home.html', controller: 'LandingController' });
     $routeProvider.when('/soylib', { templateUrl: 'views/soylib.html', controller: 'LandingController' });
     $routeProvider.when('/nodechat', { templateUrl: 'views/nodechat.html', controller: 'LandingController' });
     $routeProvider.when('/rook', { templateUrl: 'views/rook.html', controller: 'LandingController' });
     $routeProvider.when('/dashboard', { templateUrl: 'views/dashboard.html', controller: 'LandingController' });
     $routeProvider.when('/theremin', { templateUrl: 'views/theremin.html', controller: 'LandingController' });
-    $routeProvider.otherwise({ redirectTo: '/' });
+    $routeProvider.otherwise({ redirectTo: '/home' });
 }]);
--- app/scripts/controllers/MenuController.js
+++ app/scripts/controllers/MenuController.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('portfolio.controllers').
-    controller('MenuController', ['$scope', function ($scope) {
+    controller('MenuController', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $scope.projects = [
+            { name: 'Home', page: 'home' },
             { name: 'SoyLib', page: 'soylib' },
             { name: 'NodeChat', page: 'nodechat' },
             { name: 'Rook', page: 'rook' },
@@ -10,7 +11,21 @@ angular.module('portfolio.controllers').
             { name: 'Theremin', page: 'theremin' }
         ];
 
-        $scope.alert = function () {
+        $rootScope.sidePanelIsOpen = false;
+
+        $rootScope.openSidePanel = function () {
+            $rootScope.sidePanelIsOpen = true;
+        }
+
+        $rootScope.closeSidePanel = function () {
+            $rootScope.sidePanelIsOpen = false;
+        }
+
+        $rootScope.toggleSidePanel = function () {
+            $rootScope.sidePanelIsOpen = !$rootScope.sidePanelIsOpen;
+        }
+
+        $rootScope.alert = function () {
             alert('hello');
         }
     }]);
