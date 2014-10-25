8e1f7410d18e1471be3ad397e18f2434c5585c3a
Nathan Friend <contact@nathanfriend.com>
2014-10-25 17:11:40 -0500

    more inspirograph content, started nodechat content

--- Portfolio/scripts/controllers/MenuController.js
+++ Portfolio/scripts/controllers/MenuController.js
@@ -6,8 +6,8 @@ angular.module('portfolio.controllers').
     controller('MenuController', ['$scope', '$rootScope', '$route', '$location', function ($scope, $rootScope, $route, $location) {
         $scope.projects = [
             { name: 'Home', page: 'home' },
-            { name: 'SoyLib', page: 'soylib' },
             { name: 'Inspirograph', page: 'inspirograph' },
+            { name: 'SoyLib', page: 'soylib' },
             { name: 'NodeChat', page: 'nodechat' },
             { name: 'Rook', page: 'rook' },
             { name: 'PSI Dashboard', page: 'dashboard' },
