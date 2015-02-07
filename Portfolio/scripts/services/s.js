d6a1ae09eafc3ae4140efc2d1464e367d9f456bd
Nathan Friend <hello@nathanfriend.io>
2015-02-06 20:44:38 -0600

    added stuff

--- Portfolio/scripts/services/s.js
+++ Portfolio/scripts/services/s.js
@@ -1,7 +1,7 @@
 
 angular.module('portfolio.services')
     .factory('s', function () {
-        var l = { 'dmw': 'Pneartvr Zryyba', 'xdu': 'Vbjn Fgngr', 'tdm': 'Trbetvn Grpu', 'emd': 'Havirefvgl bs Jnfuvatgba', 'txd': 'ZVG', 'mid': 'Orexryrl', 'dim': 'Fgnasbeq', 'idm': 'Havirefvgl bs Vyyvabvf', 'zvpebfbsg': 'Zvpebfbsg', 'mim': 'Zvpebfbsg' };
+        var l = { 'dmw': 'Pneartvr Zryyba', 'xdu': 'Vbjn Fgngr', 'tdm': 'Trbetvn Grpu', 'emd': 'Havirefvgl bs Jnfuvatgba', 'txd': 'ZVG', 'mid': 'Orexryrl', 'dim': 'Fgnasbeq', 'idm': 'Havirefvgl bs Vyyvabvf', 'zvpebfbsg': 'Zvpebfbsg', 'mim': 'Zvpebfbsg', 'fgnpxrkpunatr': 'Fgnpx Rkpunatr' };
         var r = function (s) { return s.replace(/[a-zA-Z]/g, function (c) { return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26); }); };
         return function (i) { var o = l[r(i)]; return o ? (', ' + r(o)) : ''; }
     });
